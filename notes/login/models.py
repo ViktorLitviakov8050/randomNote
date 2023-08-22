cd6ed5363184f58bf21e4075dc7819c1026baa0a
Viktor <888viktor2012@gmail.com>
2023-08-22 17:43:02 +0100

    User model, Auth view

--- notes/login/models.py
+++ notes/login/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+from django.db import models
+
+
+class User(models.Model):
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    token = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
--- notes/login/views.py
+++ notes/login/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
+from .models import User 
+from django.http import JsonResponse
+from django.core.exceptions import BadRequest
 
 
 @csrf_exempt 
@@ -8,6 +11,16 @@ def auth(request):
     if request.method == "GET":
         return HttpResponse("Enter email and pass")    
     elif request.method == "POST":
-        print(request.body)
-        return HttpResponse("Success")    
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        if email and password:
+            try:
+                user = User.objects.get(email=email)
+            except User.DoesNotExist:
+                user = User(email=email, password=password)
+            return JsonResponse({'token':'qewreqwreq'})
+
+        raise BadRequest('Invalid email or password.')
+   
+
 
