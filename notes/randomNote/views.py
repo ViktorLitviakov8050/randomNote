dcf8b66f8fd8453d9513bb70463faa6b2a428579
Viktor <888viktor2012@gmail.com>
2023-08-29 19:46:52 +0100

    remove commented code

--- notes/randomNote/views.py
+++ notes/randomNote/views.py
@@ -1,7 +1,6 @@
 from rest_framework import views
 from login.models import User 
 from django.http import JsonResponse
-from django.db import IntegrityError
 from uuid import UUID
 
 
@@ -14,19 +13,3 @@ class GetRandomNoteView(views.APIView):
         except (ValueError, User.DoesNotExist):
             return JsonResponse({"error:": "Invalid token"}, status=401)
         return JsonResponse({"note": "randomnote"})
-
-
-        # import pdb; pdb.set_trace()
-
-        # email = request.POST.get("email")
-        # password = request.POST.get("password")
-        # try:
-        #     user = User.objects.get(email=email, password=password)
-        # except User.DoesNotExist:
-        #     user = User(email=email, password=password)
-        #     try:
-        #         user.save()
-
-        #     except IntegrityError:
-        #         return JsonResponse({"error:": "Invalid password"}, status=400)
-        # return JsonResponse({'token':user.token})
