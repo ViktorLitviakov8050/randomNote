1e9936d2e2d895337b8538d8597c1ec03866c62d
graveetone <zastavskijs@gmail.com>
2023-09-16 14:06:14 +0300

    makes helper method more clear

--- notes/helpers.py
+++ notes/helpers.py
@@ -7,7 +7,8 @@ def camel_snake_case(camel_case_string):
 
 
 def convert_time_from_usec_to_timezoned_utc(usec_time):
-    utc_time = datetime.datetime.utcfromtimestamp(usec_time/1000000)
+    timestamp_in_seconds = usec_time / 1000000
+    utc_time = datetime.datetime.utcfromtimestamp(timestamp_in_seconds)
     timezoned_utc_time = timezone.make_aware(utc_time, timezone.get_current_timezone())
 
     return timezoned_utc_time
