6760b5446a886b4a9821d4a74a2ef83cfd6b7374
Viktor <888viktor2012@gmail.com>
2023-09-20 20:02:19 +0100

    updates settings to create attachments folder if not exist

--- notes/notes/settings.py
+++ notes/notes/settings.py
@@ -14,9 +14,13 @@ from pathlib import Path
 from dotenv import load_dotenv
 import os
 
+    
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+ATTACHMENTS_DIR = os.path.join(BASE_DIR, "Attachments")
 
+if not os.path.exists(ATTACHMENTS_DIR):
+    os.mkdir(ATTACHMENTS_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
