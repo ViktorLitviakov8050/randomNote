e1c5ebc7bb3a59c1ebc14507a3b46c340ab5e5ff
Viktor <888viktor2012@gmail.com>
2023-09-20 19:57:02 +0100

    implements copying file attachments

--- notes/helpers.py
+++ notes/helpers.py
@@ -1,6 +1,7 @@
 import re
 import datetime
 from django.utils import timezone
+import shutil
 
 def camel_snake_case(camel_case_string):
     return re.sub(r'(?<!^)(?=[A-Z])', '_', camel_case_string).lower()
@@ -12,3 +13,10 @@ def convert_time_from_usec_to_timezoned_utc(usec_time):
     timezoned_utc_time = timezone.make_aware(utc_time, timezone.get_current_timezone())
 
     return timezoned_utc_time
+
+
+def copy_file(source, destination):
+    shutil.copyfile(source, destination)
+    
+
+
