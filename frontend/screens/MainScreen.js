df68b5a5b1cfceef85f3c568152cb850c8ea1930
Viktor <888viktor2012@gmail.com>
2023-09-27 19:54:46 +0100

    connects frontend and backend through request

--- frontend/screens/MainScreen.js
+++ frontend/screens/MainScreen.js
@@ -1,11 +1,28 @@
 import { View, Text, Image, Dimensions, StyleSheet, Pressable, ScrollView } from 'react-native'
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import note from '../mocks/note'
 import ImagesCarousel from '../components/ImagesCarousel'
 import Button from '../components/Button'
 
+const getRandomNote = async (callback) => {
+    try {
+        const response = await fetch(
+            'http://127.0.0.1:8000/notes/getrandomnote',
+            );
+            const json = await response.json();
+            callback(json);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
 const MainScreen = () => {
-    const images = note.images.map((imageURL) => {
+    const [note, setNote] = useState({})
+    useEffect(() => {
+            getRandomNote(setNote)
+        },[]);
+        
+    const images = note.images?.map((imageURL) => {
         return (
             <Image
                 key={imageURL}
@@ -15,16 +32,23 @@ const MainScreen = () => {
             />
         )
     })
+    const labels = note.labels?.map((label) => {
+        return (
+            <Text
+                key={label.name}
+            >{label.name}</Text>
+        )
+    })
     return (
         <View style={styles.container}>
             <ScrollView contentContainerStyle={styles.scrollable_container}>
-                <Text style={styles.noteText}>{note.text}</Text>
+                <Text style={styles.noteText}>{note.title}</Text>
+                <Text style={styles.noteText}>{note.text_content}</Text>
+                {labels?.map(label=>label)}
                 <ImagesCarousel data={images} />
             </ScrollView>
             <View style={styles.buttons_container}>
-                <Button color='blue' title="Edit" onPress={() => alert('Edit')} />
-                <Button color='green' title="Next" onPress={() => alert('Next')} />
-                <Button color='red' title="Delete" onPress={() => alert('Delete')} />
+                <Button color='green' title="Next" onPress={() => getRandomNote(setNote)} />
             </View>
         </View>
     )
